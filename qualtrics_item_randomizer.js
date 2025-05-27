89e56a49d5c117ba8e48426d2f6fb01497108719
Jaehyun Song <tintstyle@gmail.com>
2025-05-27 13:32:21 +0900

    Update qualtrics_item_randomizer.js

--- qualtrics_item_randomizer.js
+++ qualtrics_item_randomizer.js
@@ -3,7 +3,7 @@
    - 各項目は""で囲み、カンマ（,）で区切る
 2. 処置群の項目をtreatArray内に入力
    - 書き方は統制群と同じ
-   - 処置群が2つの場合、["Sensitive Item 1", "Sensitive Item 2"] のように書く
+   - 処置群が2つの場合、["Sensitive Item 1", "Sensitive Item 2"]; のように書く
 3. Qualtricsの最初の質問文にこのJavaScriptを貼り付ける（内）
 4. Survey Flowの最初のブロックとしてEmbeded Dataを入れ、Group、List_1、List_2、...を入れる。
    - 統制項目の数 + 1まで。たとえば、統制項目が4個なら List_5 まで作成
@@ -12,7 +12,7 @@ var controlArray = ["Control Item 1",
                     "Control Item 2", 
                     "Control Item 3", 
                     "Control Item 4"];
-var treatArray   = ["Sensitive Item 1", "Sensitive Item 2"]
+var treatArray   = ["Sensitive Item 1"];
 
 // 以下からは修正しなくても良い
 var group = Math.floor(Math.random() * (treatArray.length + 1));
